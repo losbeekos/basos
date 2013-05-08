5135f1caed34b17ab4556c44aaf50174671a1783
Jeroen van Beek <losbeekos@gmail.com>
2013-05-08 17:15:33 +0200

    Added OG, twitter, MS tags

--- index.php
+++ index.php
@@ -1,32 +1,52 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Page title</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width" />
 
-        <link rel="stylesheet" href="css/main.css">
-
-        <link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png">
-        <link rel="apple-touch-icon" href="apple-touch-icon-precomposed.png">
-        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="stylesheet" href="css/main.css" />
         
-         <?php
-         /**
-         *
-         * Custom h5bp build with:
-         *     - Matchmedia (https://github.com/paulirish/matchMedia.js)
-         *     - RespondJS (https://github.com/scottjehl/Respond)
-         *
-         **/
-         ?>
+        <?php
+            /* Images */
+        ?>
+        <link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png" />
+        <link rel="apple-touch-icon" href="apple-touch-icon-precomposed.png" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="image-144x144px.png"/>
+
+        <?php
+            /* Open Graph tags */
+        ?>
+        <meta property="og:image" content="image-50x50px.png" />
+        <meta property="og:title" content="Page title" />
+        <meta property="og:url" content="Current URL" />
+        <meta property="og:site_name" content="Site name" />
+        <meta property="og:type" content="website" />
+
+        <?php
+            /* Twitter cards */
+        ?>
+        <meta property="twitter:card" content="app" />
+        <meta property="twitter:site" content="@twitteraccount" />
+        
+        <?php
+            /**
+             *
+             * Custom h5bp build with:
+             *     - Matchmedia (https://github.com/paulirish/matchMedia.js)
+             *     - RespondJS (https://github.com/scottjehl/Respond)
+             *
+            **/
+        ?>
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
         <!--[if (gte IE 6)&(lte IE 8)]>
             <script src="js/plugins/selectivizr/selectivizr.min.js"></script>
