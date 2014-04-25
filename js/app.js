48f658d3382555d1eb4cc2c772f99378f8b8ec79
Jeroen van Beek <losbeekos@gmail.com>
2014-04-25 11:40:52 +0200

    Multiple parsley validators.

--- js/app.js
+++ js/app.js
@@ -560,41 +560,44 @@ var modules = {
 
     formValidation: {
         init: function(){
-            var self = this;
+            var self = this,
+                parsleyOptions = {
+                    trigger: 'change',
+                    successClass: 'form__input--success',
+                    errorClass: 'form__input--error',
+                    errors: {
+                        classHandler: function (element, isRadioOrCheckbox){
+                            var $element = $(element);
+
+                            if ($element[0].localName === 'select') {
+                                $($element[0].offsetParent).closest('.form__input').addClass('form__input--select-validated');
+                            }
+
+                            if (isRadioOrCheckbox) {
+                                return $element.closest('.form__input-list');
+                            } else {
+                                return $element.closest('.form__input');
+                            }
+                        },
+
+                        container: function (element, isRadioOrCheckbox) {
+                            var $container = element.closest('.form__input');
+
+                            if ($container.length === 0) {
+                                $container = $("<ul class='parsley-container'></ul>").append($container);
+                            }
+
+                            return $container;
+                        }
+                    }
+                };
 
             if(settings.formValidation.$el.length > 0){
                 yepnope.injectJs(basosJsPath + 'plugins/jquery.parsley/i18n/messages.nl.js' + settings.version, function () {
                     yepnope.injectJs(basosJsPath + 'plugins/jquery.parsley/parsley.js' + settings.version,
                         function(){
-                            settings.formValidation.$el.parsley({
-                                trigger: 'change',
-                                successClass: 'form__input--success',
-                                errorClass: 'form__input--error',
-                                errors: {
-                                    classHandler: function (element, isRadioOrCheckbox){
-                                        var $element = $(element);
-
-                                        if ($element[0].localName === 'select') {
-                                            $($element[0].offsetParent).closest('.form__input').addClass('form__input--select-validated');
-                                        }
-
-                                        if (isRadioOrCheckbox) {
-                                            return $element.closest('.form__input-list');
-                                        } else {
-                                            return $element.closest('.form__input');
-                                        }
-                                    },
-
-                                    container: function (element, isRadioOrCheckbox) {
-                                        var $container = element.closest('.form__input');
-
-                                        if ($container.length === 0) {
-                                            $container = $("<ul class='parsley-container'></ul>").append($container);
-                                        }
-
-                                        return $container;
-                                    }
-                                }
+                            settings.formValidation.$el.each(function () {
+                                $(this).parsley(parsleyOptions);
                             });
                         });
                 });
