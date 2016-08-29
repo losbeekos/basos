2fcdfb742048ac0e293764784e3f1863f2c2ccda
Jeroen van Beek <losbeekos@gmail.com>
2016-08-29 14:48:06 +0200

    UnjQuerified btnDropdown

--- assets/dist/js/app.js
+++ assets/dist/js/app.js
@@ -4225,36 +4225,51 @@ app.affix = {
 };
 app.btnDropdown = {
     init: function() {
-        app.settings.$body.on('click', '[data-btn-dropdown-toggle]', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
 
-            var $this = $(this),
-                $btnDropdown = $this.closest('.btn-dropdown');
-
-            if ($btnDropdown.hasClass('btn-dropdown--open')) {
-                $btnDropdown.removeClass('btn-dropdown--open');
-            } else {
-                $('.btn-dropdown--open').removeClass('btn-dropdown--open');
-                $btnDropdown.addClass('btn-dropdown--open');
-            }
-        });
-
-        app.settings.$body.on('keydown', function(event){
-            if (event.keyCode === 27) {
-                $('.btn-dropdown--open').removeClass('btn-dropdown--open');
-            }
-        });
-
-        app.settings.$body.on('click', '.btn-dropdown__dropdown, .btn-dropdown__list', function (event) {
-            var allowProp = $(this).attr("data-btn-dropdown");
-            if (allowProp !== "allowPropagation") {
+        // Dropdown toggler
+        document.querySelectorAll('[data-btn-dropdown-toggle]').forEach(function (toggle) {
+            toggle.addEventListener('click', function (event) {
+                event.preventDefault();
                 event.stopPropagation();
-            }
+
+                btnDropdown = this.closest('.btn-dropdown');
+
+                if (btnDropdown.classList.contains('btn-dropdown--open')) {
+                    btnDropdown.classList.remove('btn-dropdown--open');
+                } else {
+                    app.btnDropdown.closeOpenDropdown();
+                    btnDropdown.classList.add('btn-dropdown--open');
+                }
+            });
         });
 
-        app.settings.$body.on('click', function () {
-            $('.btn-dropdown--open').removeClass('btn-dropdown--open');
+        // Do not close dropdown on dropdown content clicks
+        document.querySelectorAll('.btn-dropdown__dropdown, .btn-dropdown__list').forEach(function (btn) {
+            btn.addEventListener('click', function (event) {
+                var allowProp = btn.getAttribute('data-btn-dropdown');
+
+                if (allowProp !== 'allowPropagation') {
+                    event.stopPropagation();
+                }
+            });
+        });
+
+        // Close all dropdowns on escape keydown
+        document.onkeydown = function (event) {
+            if (event.keyCode === 27) {
+                app.btnDropdown.closeOpenDropdown();
+            }
+        };
+
+        // Close all dropdowns on body click
+        document.body.addEventListener('click', function () {
+            app.btnDropdown.closeOpenDropdown();
+        });
+    },
+
+    closeOpenDropdown: function () {
+        document.querySelectorAll('.btn-dropdown--open').forEach(function (openDropdown) {
+            openDropdown.classList.remove('btn-dropdown--open');
         });
     }
 
